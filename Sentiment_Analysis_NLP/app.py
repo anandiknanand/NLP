cacde82b8b666ced50f89fe3008b43f8e4bcce6d
anandiknanand <15522@yenepoya.edu.in>
2024-06-24 22:44:20 +0530

    a

---
+++ Sentiment_Analysis_NLP/app.py
@@ -0,0 +1,16 @@
+import joblib
+#import streamlit as st
+import streamlit as st
+
+saved_model = joblib.load("sentiment-model.pkl")
+
+sentiment_labels = {0: "Negative", 1: "Positive"}
+
+st.title("Sentiment Analysis")
+user_input = st.text_input("Enter a sentence:")
+
+if st.button("Predict"):
+    prediction = saved_model.predict([user_input])
+    st.info(f"Predicted Sentiment: {sentiment_labels[prediction[0]]}")
+    
+
